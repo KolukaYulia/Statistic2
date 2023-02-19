f3465dc4dfedb16cfca968f3d7c8cb4a9f16bd3e
“Koluka” <koluka78@mail.ru>
2023-02-19 22:18:35 +0300

    AddServiceTest

---
+++ src/test/java/StatisticsServiceTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatisticsServiceTest {
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMaxInMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 60, 8, 6, 11, 11, 12};
+        long expected = 60;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+}
